e3aa042502e6abcaf84c0d442995c22148722743
LuizHenriqueT1 <luiz.hesoaress@gmail.com>
2023-05-16 15:24:15 -0300

    Autorizacao com JWT

--- src/main/java/com/rick/cursomc/configurations/SecurityConfig.java
+++ src/main/java/com/rick/cursomc/configurations/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.rick.cursomc.configurations;
 
 import com.rick.cursomc.security.JWTAuthenticationFilter;
+import com.rick.cursomc.security.JWTAuthorizationFilter;
 import com.rick.cursomc.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -46,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     };
 
     private static final String[] PUBLIC_MATCHERS_POST = {
-            "/clientes",
+            "/clientes/**",
             "/auth/forgot/**"
     };
 
@@ -64,6 +65,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
